66c5c720b3e2d124bbce74d2a9f24bb10f79df7d
Cong-Thi <trancongthi@gmail.com>
2022-08-29 18:14:39 +0700

    Bai Tap

---
+++ app/services/accountAPI.js
@@ -0,0 +1,43 @@
+function apiGetAccounts(searchTerm) {
+    return axios({
+           url: "https://62f50939535c0c50e76847f6.mockapi.io/education",
+           method: "GET",
+           params: {
+               name: searchTerm
+           }
+       });
+   }
+   
+   function apiAddAccount(account) {
+       return axios({
+              url: "https://62f50939535c0c50e76847f6.mockapi.io/education",
+              method: "POST",
+              data: account,
+          });
+      }
+   
+      function apiDeleteAccount(accountID) {
+       return axios({
+              url: `https://62f50939535c0c50e76847f6.mockapi.io/education/${accountID}`,
+              method: "DELETE",
+          });
+      }
+   
+      function apiGetAccountById(accountID) {
+       return axios({
+              url: `https://62f50939535c0c50e76847f6.mockapi.io/education/${accountID}`,
+              method: "GET",
+          });
+      }
+   
+      function apiUpdateAccount(accountID, account) {
+       return axios({
+              url: `https://62f50939535c0c50e76847f6.mockapi.io/education/${accountID}`,
+              method: "PUT",
+              data: account,
+          });
+      }
+
+     
+   
+   
